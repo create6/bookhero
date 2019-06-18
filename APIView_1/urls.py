685b28a338e3fe211dbc509c6808552dd65f8ea0
xwp <xwp_fullstack@163.com>
2019-06-18 20:51:03 +0800

    udpate

--- APIView_1/urls.py
+++ APIView_1/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-	url(r'^demo1/$', views.Demo1APIView.as_view())
+	url(r'^demo1/$', views.Demo1APIView.as_view()),
+	url(r'^demo2/$', views.Demo2APIView.as_view()),
+
 
 
 ]
--- APIView_1/views.py
+++ APIView_1/views.py
@@ -1,11 +1,9 @@
-
-
 from django import http
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 #1,一级视图，APIView之request
-from rest_framework.views import APIView
-
-
 class Demo1APIView(APIView):
 	def get(self,request):
 		print(request.query_params)
@@ -16,4 +14,12 @@ class Demo1APIView(APIView):
 		print(request.data)
 		return http.HttpResponse("老子曰：老子愿意！")
 
+#2,一级视图，APIView之response
+class Demo2APIView(APIView):
+	def get(self,request):
 
+		return Response("登高而呼，声非加疾也，而闻者彰")
+
+	def post(self,request):
+		print(request.data)
+		return http.HttpResponse("老子曰：老子愿意！")
